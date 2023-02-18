200b71f5479b3ce136ec6c8bfe67c71e8273a395
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2023-02-18 10:19:42 +0530

    CRUD on products

---
+++ db.js
@@ -0,0 +1,16 @@
+const sqlite3 = require("sqlite3");
+
+const db = new sqlite3.Database(
+  "./data.db",
+  sqlite3.OPEN_READWRITE,
+  (error) => {
+    if (error) {
+      console.log("DB ERROR....")
+      console.error(error);
+    } else {
+      console.log("DB Connected...")
+    }
+  }
+);
+
+module.exports = db;
